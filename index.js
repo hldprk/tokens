2c5fb23ae7554d2ec7a4197ad21dbcf1da75462b
Holden Perkins <hldprk@gmail.com>
2023-03-20 13:50:13 -0400

    added `index.js`, restructured exports
    Signed-off-by: Holden Perkins <hldprk@gmail.com>

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+import Many from './many.js';
+import Choice from './choice.js';
+import Token from './token.js';
+import Sequence from './sequence.js';
+import State from './state.js';
+import Syntax from './syntax.js';
+
+const tokens = { Many, Choice, Token, Sequence, State, Syntax };
+
+export default tokens;
+
